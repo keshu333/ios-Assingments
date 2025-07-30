fdd7e2082ab248139bf31b43c68f597cb3cd92a4
keshav <153003532+keshu333@users.noreply.github.com>
2025-07-30 09:23:32 +0530

    Add files via upload

---
+++ viewcontrollers/viewcontrollers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewcontrollers
+//
+//  Created by Student on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("View is loaded")
+    }
+
+
+}
+
